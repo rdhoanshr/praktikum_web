5467c570aa0ffcc146c1dbf1efbeb9f109017870
unknown <anshori706@gmail.com>
2022-02-12 21:36:31 +0800

    Membuat Perintah Update Data

--- partials/routes.php
+++ partials/routes.php
@@ -46,6 +46,10 @@ if (isset($_GET['page'])) {
 			file_exists('pages/admin/bagiancreate.php') ? include
 				'pages/admin/bagiancreate.php' : include "pages/404.php";
 			break;
+		case 'bagianupdate':
+			file_exists('pages/admin/bagianupdate.php') ? include
+				'pages/admin/bagianupdate.php' : include "pages/404.php";
+			break;
 
 		default:
 			include "pages/404.php";
