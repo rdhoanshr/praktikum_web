d35bcb8f93353b2bbd0fc2b65a23e7ade0bb2cf6
unknown <anshori706@gmail.com>
2022-02-12 15:17:55 +0800

    Membuat Halaman Ubah Data

--- partials/routes.php
+++ partials/routes.php
@@ -1,25 +1,27 @@
-<?php 
-if(isset($_GET['page'])){
+<?php
+if (isset($_GET['page'])) {
 	$page = $_GET['page'];
 	switch ($page) {
 		case '':
 		case 'home':
-		file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
+			file_exists('pages/home.php') ? include 'pages/home.php' : include "pages/404.php";
 			break;
 		case 'lokasiread':
-		file_exists('pages/admin/lokasiread.php') ? include
-			'pages/admin/lokasiread.php' : include "pages/404.php";
+			file_exists('pages/admin/lokasiread.php') ? include
+				'pages/admin/lokasiread.php' : include "pages/404.php";
 			break;
 		case 'lokasicreate':
-		file_exists('pages/admin/lokasicreate.php') ? include 
-		'pages/admin/lokasicreate.php' : include "pages/404.php";
-		break;
-		
+			file_exists('pages/admin/lokasicreate.php') ? include
+				'pages/admin/lokasicreate.php' : include "pages/404.php";
+			break;
+		case 'lokasiupdate':
+			file_exists('pages/admin/lokasiupdate.php') ? include
+				'pages/admin/lokasiupdate.php' : include "pages/404.php";
+			break;
+
 		default:
 			include "pages/404.php";
 	}
 } else {
 	include "pages/home.php";
 }
-
- ?>
