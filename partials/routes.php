2c39965f915d016328ab2af6e06fb07a65c6aca6
unknown <anshori706@gmail.com>
2022-02-12 21:41:01 +0800

    Membuat Perintah Delete Data

--- partials/routes.php
+++ partials/routes.php
@@ -50,6 +50,10 @@ if (isset($_GET['page'])) {
 			file_exists('pages/admin/bagianupdate.php') ? include
 				'pages/admin/bagianupdate.php' : include "pages/404.php";
 			break;
+		case 'bagiandelete':
+			file_exists('pages/admin/bagiandelete.php') ? include
+				'pages/admin/bagiandelete.php' : include "pages/404.php";
+			break;
 
 		default:
 			include "pages/404.php";
