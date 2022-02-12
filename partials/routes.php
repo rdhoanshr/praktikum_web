fa27869bb9551b041a4fe6238a5031b4f9676479
unknown <anshori706@gmail.com>
2022-02-12 20:51:38 +0800

    Membuat Perintah Read Data

--- partials/routes.php
+++ partials/routes.php
@@ -38,6 +38,10 @@ if (isset($_GET['page'])) {
 			file_exists('pages/admin/jabatandelete.php') ? include
 				'pages/admin/jabatandelete.php' : include "pages/404.php";
 			break;
+		case 'bagianread':
+			file_exists('pages/admin/bagianread.php') ? include
+				'pages/admin/bagianread.php' : include "pages/404.php";
+			break;
 
 		default:
 			include "pages/404.php";
