434034a679fb25002e5925b4648cf12b375215bf
unknown <anshori706@gmail.com>
2022-02-13 11:21:27 +0800

    Membuat Halaman Tampil Data Karyawan

--- partials/routes.php
+++ partials/routes.php
@@ -54,6 +54,10 @@ if (isset($_GET['page'])) {
 			file_exists('pages/admin/bagiandelete.php') ? include
 				'pages/admin/bagiandelete.php' : include "pages/404.php";
 			break;
+		case 'karyawanread':
+			file_exists('pages/admin/karyawanread.php') ? include
+				'pages/admin/karyawanread.php' : include "pages/404.php";
+			break;
 
 		default:
 			include "pages/404.php";
