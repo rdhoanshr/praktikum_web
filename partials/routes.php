bed77ad1b22d730736918ac6ed534b6f7267b07a
unknown <anshori706@gmail.com>
2022-02-17 10:19:14 +0800

    Membuat Halaman Riwayat Jabatan

--- partials/routes.php
+++ partials/routes.php
@@ -74,6 +74,10 @@ if (isset($_GET['page'])) {
 			file_exists('pages/admin/karyawanbagian.php') ? include
 				'pages/admin/karyawanbagian.php' : include "pages/404.php";
 			break;
+		case 'karyawanjabatan':
+			file_exists('pages/admin/karyawanjabatan.php') ? include
+				'pages/admin/karyawanjabatan.php' : include "pages/404.php";
+			break;
 
 		default:
 			include "pages/404.php";
